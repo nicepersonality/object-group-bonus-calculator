9198bab2fa508ce2f4a86fa5398910b30b913af9
Mike Lynch <mjlynch2@gmail.com>
2019-08-27 16:30:45 -0500

    first pass, skeleton roughed in

--- client.js
+++ client.js
@@ -40,4 +40,64 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
+// loop over the employee array
+// use each employee object as the input to the function described below.
+// `console.log` the results of each iteration.
+let employeesBonuses = [];
+
+function doesCoolStuff() {
+  for (person of employees){
+    
+    employeesBonuses.push(generateBonusInfo(person));
+  };
+};
+
+// Write a declared function that takes in one ** Employee ** object(as an argument to the function), and returns a new ** object ** with the following properties:
+// The`name` property should contain the employee's name.
+// The`bonusPercentage` property should contain the bonus percentage the employee is to receive.See section below for calculation instructions.
+// The`totalCompensation` property should be the adjusted annual compensation(base annual + bonus)
+// The`totalBonus` should be the employee's total bonus rounded to the nearest dollar.
+
+// constructor
+
+function EmployeeBonus(name, bonusPercentage, totalCompensation, totalBonus){
+  this.name = name;
+  this.bonusPercentage = bonusPercentage;
+  this.totalCompensation = totalCompensation;
+  this.totalBonus = totalBonus;
+}
+
+function generateBonusInfo(employee){
+let employeeBonusInfo = new EmployeeBonus(
+  name = employee.name,
+  bonusPercentage = bonusPercentCalc(),
+  totalCompensation = totalCompensationCalc(),
+  totalBonus = totalBonusCalc()
+)
+
+  // 
+return employeeBonusInfo;
+};
+
+// ### Individual Bonus calculation
+//  - Those who have a rating of a 2 or below should not receive a bonus.
+// - Those who have a rating of a 3 should receive a base bonus of 4 % of their base annual income.
+// - Those who have a rating of a 4 should receive a base bonus of 6 % of their base annual income.
+// - Those who have a rating of a 5 should receive a base bonus of 10 % of their base annual income.
+// - If their employee number is 4 digits long, this means they have been with the company for longer than 15 years,
+//   and should receive an additional 5 %.
+// - However, if their annual income is greater than $65, 000, they should have their bonus adjusted down 1 %.
+// - No bonus can be above 13 % or below 0 % total.
+
+function bonusPercentCalc () {
+
+}
+
+function totalCompensationCalc () {
+
+}
+
+function totalBonusCalc (){
+
+}
 console.log( employees );
